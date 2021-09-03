089b93bfa48ccb0af7628f705e9adb08642046ed
NUGA <ngjang@kakao.com>
2021-09-03 14:31:09 +0900

    Initial Commit

---
+++ src/main/java/egovframework/board/cmmn/web/TestController.java
@@ -0,0 +1,14 @@
+package egovframework.board.cmmn.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping(value="/testList.do")
+	public String testListDo() {
+		return "test/testList";
+	}
+
+}
